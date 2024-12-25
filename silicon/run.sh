bf8ff14d99d7a9c3ad3e90bd4c96f40e027b8900
Jonáš Fiala <jonas.fiala@inf.ethz.ch>
2024-12-25 20:07:40 +0100

    Increase stack size

--- silicon/run.sh
+++ silicon/run.sh
@@ -1,3 +1,3 @@
 OUT=$(pwd)/${2%.*}
 mkdir -p $(dirname "$OUT")
-cd $(dirname "$0")/silicon && Z3_EXE=$(which z3) java -jar ./target/scala-*/silicon.jar --timeout $3 --numberOfParallelVerifiers 1 --proverLogFile "$OUT.smt2" $1
+cd $(dirname "$0")/silicon && Z3_EXE=$(which z3) java -Xss30M -jar ./target/scala-*/silicon.jar --timeout $3 --numberOfParallelVerifiers 1 --proverLogFile "$OUT.smt2" $1
