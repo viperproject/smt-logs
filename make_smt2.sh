bf4708464c960eee10b7e03a3bae5825455df6f1
Jonáš Fiala <jonas.fiala@inf.ethz.ch>
2024-12-26 02:32:11 +0100

    Testing & fix

--- make_smt2.sh
+++ make_smt2.sh
@@ -13,16 +13,16 @@ VERIFIERS="$DIRNAME/$(echo $VERIFIERS | sed "s| | $DIRNAME/|g")"
 for verifier in $VERIFIERS; do
     echo "[Running $verifier]"
     cd $verifier
+    continue # TODO: Remove this line
     ./build.sh
     VERIFIER=$(basename $verifier)
     for file in `./tests.sh`; do
-        no_prefix=$(echo $file | sed "s|.*/$VERIFIER/||g")
+        no_prefix=$(echo $file | perl -pe "s|.*?/$VERIFIER/||")
         if [ "$file" == "$no_prefix" ]; then
             echo "Could not strip prefix (.*/$VERIFIER/): $no_prefix"
             exit 1
         fi
         echo "[.smt2] $no_prefix"
         ./run.sh $file ../smt2/$VERIFIER/$no_prefix ${TIMEOUT:-10}
-        break # TODO: Remove this line
     done
 done
