3494f5473c05a8794144cb9999bb8348ed024fba
Jonáš Fiala <jonas.fiala@inf.ethz.ch>
2025-01-08 19:54:41 +0100

    Fix silicon `.smt2` file names

--- silicon/run.sh
+++ silicon/run.sh
@@ -1,6 +1,6 @@
 OUT="$(pwd)/${2%.*}"
 mkdir -p "$(dirname "$OUT")"
-ERROR=$((python "$(dirname "$0")/../viper/viper_client/client.py" -j bulk -p 50424 -f "$1" -v silicon -x="--disableCaching --timeout \"$3\" --numberOfParallelVerifiers 1 --proverLogFile \"$OUT.smt2\"" > /dev/null) 2>&1)
+ERROR=$((python "$(dirname "$0")/../viper/viper_client/client.py" -j bulk -p 50424 -f "$1" -v silicon -x="--disableCaching --timeout \"$3\" --numberOfParallelVerifiers 1 --proverLogFile \"$OUT\"" > /dev/null) 2>&1)
 if [ ! -z "$ERROR" ]; then
   echo "[ERROR] $ERROR"
   exit 1
